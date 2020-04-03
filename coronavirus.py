553a81da859c8650e3ce04c1c11548985682c1ba
Hans Fangohr <fangohr@soton.ac.uk>
2020-04-03 23:19:32 +0200

    remove last day from RKI institute data
    - not all cases / deaths registered yet; see text in code

--- coronavirus.py
+++ coronavirus.py
@@ -106,6 +106,9 @@ def get_country(country):
 
 @lru_cache(maxsize=1)
 def fetch_data_germany():
+    """Data source is https://npgeo-corona-npgeo-de.hub.arcgis.com . The text on the
+    webpage implies that the data comes from the Robert Koch Institute. """
+    
     datasource = "https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv"
     t0 = time.time()
     print(f"Please be patient - downloading data from {datasource} ...")
@@ -117,8 +120,33 @@ def fetch_data_germany():
     ## - this is too simplistic. We have fields like "Region Hannover"
     # germany['landkreis'] = germany['Landkreis'].apply(lambda s: s[3:]) 
 
-    # remember for the next call of this function
-    return germany
+    # (at least) the last data from the Robert-Koch-Institute (RKI) seems not to be
+    # fully reported the day after. For example, on 3 April, the number of cases
+    # from RKI is well below what is expected. Example:
+    #
+    # From RKI (as of evening of 2020-04-03:)
+    # 2020-03-29    62653
+    # 2020-03-30    66692
+    # 2020-03-31    72333
+    # 2020-04-01    77464
+    # 2020-04-02    79625
+    #
+    # From Johns Hopkins (as of evening of 2020-04-03:):
+    # 2020-03-29    62095
+    # 2020-03-30    66885
+    # 2020-03-31    71808
+    # 2020-04-01    77872
+    # 2020-04-02    84794
+    #
+    # So we must assume that the RKI data will be corrected later; maybe the next day.
+    #
+    # To make our plots not inaccurate, we'll remove the last data point from the RKI data:
+    g2 = germany.set_index(pd.to_datetime(germany['Meldedatum']))
+    g2.index.name = 'date'
+    last_day = g2.index.max()
+    sel = g2.index == last_day
+    cleaned = g2.drop(g2[sel].index, inplace=False)
+    return cleaned
 
 
 def germany_get_region(state=None, landkreis=None):
