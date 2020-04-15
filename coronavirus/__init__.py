9a901ea1c7217a5c006cf70373808d1095460bd6
Thomas Michelat <thomas.michelat@xfel.eu>
2020-04-15 17:45:41 +0200

    reorganize code and notebook; update travis integration

---
+++ coronavirus/__init__.py
@@ -0,0 +1,3 @@
+from .coronavirus import (
+    fetch_cases, fetch_data_germany, fetch_deaths, germany_get_region, overview
+)
---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import os.path as osp
+import re
+from setuptools import setup, find_packages
+import sys
+
+
+def get_script_path():
+    return osp.dirname(osp.realpath(sys.argv[0]))
+
+
+def read(*parts):
+    return open(osp.join(get_script_path(), *parts)).read()
+
+
+def find_version(*parts):
+    vers_file = read(*parts)
+    match = re.search(r'^__version__ = "(\d+\.\d+\.\d+)"', vers_file, re.M)
+    if match is not None:
+        return match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(name="coronavirus-2020",
+      version=find_version("coronavirus", "__init__.py"),
+      author="European XFEL GmbH",
+      author_email="da-support@xfel.eu",
+      maintainer="Hans Fangohr",
+      url="https://github.com/fangohr/coronavirus-2020",
+      description="Coronavirus-2020",
+      long_description=read("README.md"),
+      long_description_content_type='text/markdown',
+      license="BSD-3-Clause",
+      packages=find_packages(),
+      install_requires=[
+          'joblib',
+          'pandas',
+          'seaborn',
+          'matplotlib',
+          'numpy',
+          'scipy',
+          'voila',
+          'pelican',
+          'seaborn',
+      ],
+      extras_require={
+          'test': [
+              'pytest',
+              'pytest-cov',
+              'coverage',
+              'nbval',
+              'testpath',
+          ]
+      },
+      python_requires='>=3.6',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+      ]
+)
