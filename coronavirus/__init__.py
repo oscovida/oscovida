ecc2b8c90f2ed9117adb29059d54955f0a657ad5
Thomas Michelat <thomas.michelat@xfel.eu>
2020-04-16 10:46:10 +0200

    put back docker make option; fix imports; add pycodestyle to requirements

--- coronavirus/__init__.py
+++ coronavirus/__init__.py
@@ -1,8 +1,4 @@
-from .coronavirus import (
-    compute_daily_change, compute_growth_factor, fetch_cases,
-    fetch_data_germany, fetch_deaths, germany_get_region, get_country,
-    get_country_data, overview, plot_daily_change, plot_growth_factor
-)
+from .coronavirus import *
 
 
 __version__ = "0.1.0"
