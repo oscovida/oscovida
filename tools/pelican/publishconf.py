3df4e7d945a4181a254bee2755d08619e43f3105
Hans Fangohr grey laptop <hans.fangohr@xfel.eu>
2020-04-17 17:47:49 +0200

    minor correction

--- tools/pelican/publishconf.py
+++ tools/pelican/publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://oscovida.github.io/'
+SITEURL = 'https://oscovida.github.io'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
