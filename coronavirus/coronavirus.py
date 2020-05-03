4a2a323cb35c2490fd53469df4a741d9c9598b5d
Hans Fangohr <hans.fangohr@xfel.eu>
2020-05-03 15:41:57 +0200

    bug fix

--- coronavirus/coronavirus.py
+++ coronavirus/coronavirus.py
@@ -1057,10 +1057,10 @@ def overview(country, region=None, subregion=None, savefig=False):
     elif country=="Germany":   # Germany specific plots
         # On 11 April, Mecklenburg Vorpommern data was missing from data set.
         # We thus compare only against those Laender, that are in the data set:
-        germany = fetch_data_germany()
-        laender = list(germany['Bundesland'].drop_duplicates().sort_values())
-        axes_compare, res_c, red_d = make_compare_plot_germany((region, subregion),
-                                                               compare_with_local=laender)
+        # germany = fetch_data_germany()
+        # laender = list(germany['Bundesland'].drop_duplicates().sort_values())
+        axes_compare, res_c, red_d = make_compare_plot_germany((region, subregion))
+                                                               # compare_with_local=laender)
         return_axes = np.concatenate([axes, axes_compare])
     elif country=="US" and region is not None:
         # skip comparison plot for the US states at the moment
