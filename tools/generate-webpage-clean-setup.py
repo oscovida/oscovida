fac8d75d06148a65bb8acef7d9e490ae5aafbb78
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-08-20 09:52:57 +0200

    Delete old utility scripts

--- tools/generate-webpage-clean-setup.py
+++
@@ -1,8 +0,0 @@
-import oscovida as osc
-
-# force download of new data
-osc.clear_cache()
-
-# clear all metadata entries (cache used to compose markdown after html notebooks have been created)
-osc.MetadataRegion.clear_all()
-
