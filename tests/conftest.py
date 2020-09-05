82f6cc892723c24c57f56b485fe96beb5908eaab
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-09-05 14:19:10 +0200

    Add static cache option

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,16 @@ import pytest
 import oscovida.covid19dh
 
 
+def pytest_addoption(parser):
+    #  This tells the `mock_cache_dir` fixture to set the cache directory
+    #  to a static location instead of a temporary one. With this enabled
+    #  the data files can be reused and do not have to be downloaded, this
+    #  means that the download tests are no longer performed but that the
+    #  tests can run much faster. Useful for mutation testing
+    parser.addoption("--static-cache", action="store_true", default=False)
+    parser.addoption("--disable-level-3", action="store_true", default=False)
+
+
 @pytest.fixture()
 def mock_cache_dir_fresh(tmp_path):
     cache_dir = os.path.join(tmp_path, "cache_dir")
@@ -25,8 +35,15 @@ def mock_cache_dir_fresh(tmp_path):
 
 
 @pytest.fixture(scope="session")
-def mock_cache_dir(tmp_path_factory):
-    cache_dir = tmp_path_factory.mktemp("cache_dir")
+def mock_cache_dir(tmp_path_factory, request):
+    static_cache = request.config.getoption("--static-cache")
+
+    if static_cache:
+        test_dir = os.path.dirname(os.path.abspath(__file__))
+        cache_dir = os.path.join(test_dir, "test_data", ".cache")
+    else:
+        cache_dir = tmp_path_factory.mktemp("cache_dir")
+
     with mock.patch('oscovida.covid19dh.CACHE_DIR', cache_dir):
         #  See comments for `mock_cache_new` fixture
         defaults = list(oscovida.covid19dh.get.__defaults__)
