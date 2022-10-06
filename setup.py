09b298eff29f8684cf907a3899bdb1fb983dc13c
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2022-10-06 17:00:29 +0200

    Update pins, add constraints file (#308)

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(name="oscovida",
           'pelican',
           'pelican-jupyter',
           'seaborn',
-          'tabulate<0.8.8',  # Pinned until release with https://github.com/astanin/python-tabulate/pull/111 is made
+          'tabulate>=0.8.9',
           'tqdm',
           'ipywidgets',
           'ipynb_py_convert',
